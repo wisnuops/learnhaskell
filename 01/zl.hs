049a36b7ab0d81577782ed6bf0535f3de4d01033
wisnuops <wisnuops@zeniuseducation.com>
2015-10-01 02:15:13 +0700

    sdfsd
    sdf

---
+++ 01/zl.hs
@@ -0,0 +1,54 @@
+module Zl where
+import Data.List
+
+-- 01 Coba jawab pertanyaan cupu yang satu ini, untuk ngetest kalo sistem udah jalan
+-- Jika Un=n dan Sn menyatakan ∑k=1nUk maka S100=?
+
+satu 1 = 1
+satu n = n + satu (n-1)
+
+no1 = satu 100 -- Hasilnya 5050
+
+--- 02 Sesuai dengan definisi |x| yang terkenal itu, maka berapa nilai dari:
+--- |x+1|+|x−2|+|x+3|+|x−4|+...+|x−100|
+--- Untuk nilai x = 50
+
+mutlak x
+  | x >= 0 = x
+  | x < 0 = -x
+
+minIfEven x
+  | (mod x 2 == 0) = (-1)*x
+  | otherwise = x
+
+dua x 1 = mutlak (x + 1)
+dua x n = (dua x (n-1)) + mutlak (x + minIfEven n)
+
+no2 = dua 50 100 -- Hasilnya 6250
+
+-- 03 Berapakah 1209380934 jika dituliskan di basis 6?
+
+listBaseTentoSix 0 = []
+listBaseTentoSix x = listBaseTentoSix (div x 6) ++ [mod x 6]
+
+no3 = listBaseTentoSix 1209380934 -- Hasilnya [3,2,0,0,0,1,1,2,1,4,5,0] inputnya: 320001121450
+
+-- 04 baseTenToSix x = a +
+-- listBaseTentoSix x
+
+factorial 0 = 1
+factorial n = n*factorial (n-1)
+
+fungsiP x
+  | x < 10 = factorial x
+  | x < 100 = factorial (mod x 10) + factorial (div x 10)
+  | x < 1000 = factorial (mod x 10) + factorial (mod (div x 10) 10) + factorial (div x 100)
+  | x < 10000 = factorial (mod x 10) + factorial (mod (div x 10) 10) + factorial (mod (div x 100) 10) + factorial (div x 1000)
+  | otherwise = 0
+
+empat 1 = fungsiP 1
+empat x = fungsiP x + empat (x-1)
+
+no4 = empat 1000 -- Hasilnya 122734093
+
+-- testing = fungsiP 1 + fungsiP 2 + fungsiP 3 + fungsiP 4 + fungsiP 5 + fungsiP 6 + fungsiP 7
